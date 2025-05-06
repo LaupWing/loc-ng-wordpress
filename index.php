76c477dd69c1613a830ee5661663b203b3088300
laupwing <laupwing@gmail.com>
2025-05-06 14:46:54 +0200

    Update button text in index.php from "Read the Blog" to "Subscribe" for clarity

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ get_header()
     <div class="mt-8 flex flex-wrap gap-4 md:!text-lg" data-fade="4">
         <div class="group relative flex">
             <div class="absolute -inset-0.5 animate-pulse rounded blur from-custom-green bg-gradient-to-r to-custom-purple opacity-75 transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-            <div class="gradient-animation-border shadow hover:scale-[1.03] active:scale-[0.97] motion-safe:transform-gpu motion-reduce:hover:scale-100 transition duration-100 scale-100 "><button class="rounded font-bold px-4 scale-100 text-gray-600 disabled:bg-gray-200 dark:text-gray-200 dark:disabled:bg-gray-700  bg-white py-[7px]"><a href="/blog">Read the Blog</a></button></div>
+            <div class="gradient-animation-border shadow hover:scale-[1.03] active:scale-[0.97] motion-safe:transform-gpu motion-reduce:hover:scale-100 transition duration-100 scale-100 "><button class="rounded font-bold px-4 scale-100 disabled:bg-gray-200  dark:disabled:bg-gray-700  bg-white py-[7px]"><a href="/blog">Subscribe</a></button></div>
         </div><a href="/about"><button class="rounded font-bold px-4 scale-100 bg-light dark:bg-dark text-gray-600 disabled:bg-gray-200 dark:text-gray-200 dark:disabled:bg-gray-700  py-2 border border-gray-300 dark:border-gray-600 focus:outline-none focus-visible:ring focus-visible:ring-accent shadow-sm hover:scale-[1.03] active:scale-[0.97] motion-safe:transform-gpu motion-reduce:hover:scale-100 transition duration-100">Learn more about me</button></a>
     </div>
 </div>
