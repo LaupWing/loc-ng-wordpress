745c4a1fdd903a29283dbb3d2fa4da1d13f1b1f0
laupwing <laupwing@gmail.com>
2025-05-07 19:10:32 +0200

    Add archive template for displaying featured products

---
+++ archive-product.php
@@ -0,0 +1,55 @@
+<?php
+get_header()
+?>
+<div class="custom-container flex items-start flex-col">
+
+    <section class="flex flex-col py-8">
+        <div class="flex flex-col py-6">
+            <span class="text-slate-400 uppercase text-sm tracking-wider">Featured Products</span>
+            <h3 class="transition-colors text-5xl pb-2 font-bold gradient-animation-slow bg-clip-text text-transparent">
+                Actually useful stuff
+            </h3>
+            <p class="max-w-4xl text-gray-700 md:text-lg 2xl:text-xl" data-fade="3">I possess a strong enthusiasm for both programming and fitnes.</p>
+        </div>
+        <ul class="grid mt-4 grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-6">
+            <?php
+            $args = array(
+                "post_type" => "product",
+                "post_status" => "publish",
+                "posts_per_page" => "5",
+                "order" => "DESC",
+                "orderby" => "date",
+
+            );
+            $query = new WP_Query($args);
+            if ($query->have_posts()) :
+                while ($query->have_posts()) : $query->the_post(); ?>
+                    <li>
+                        <a class="flex flex-col gap-2 relative" href="<?php the_permalink(); ?>">
+                            <?php if (has_post_thumbnail()) : ?>
+                                <img class="aspect-video rounded-lg object-cover" src="<?php the_post_thumbnail_url() ?>" alt="thumbnail photo">
+
+                            <?php endif; ?>
+                            <h2 class="text-3xl text-gray-800 font-bold"><?php the_title(); ?></h2>
+                            <!-- get 100 charcaters of the contetn -->
+                            <p class="text-slate-400 text-lg">
+                                <?php
+                                $content = get_the_content();
+                                $content = strip_tags($content);
+                                echo substr($content, 0, 100);
+                                ?>
+                            </p>
+                        </a>
+                    </li>
+                <?php endwhile;
+            else :
+                ?>
+                <li>No recent posts found.</li>
+            <?php endif;
+            wp_reset_postdata(); ?>
+        </ul>
+    </section>
+</div>
+<?php
+get_footer()
+?>
