39b053f0724391c4fdc9f6e72ab91f8a69412212
laupwing <laupwing@gmail.com>
2025-05-09 08:43:30 +0200

    Implement code changes to enhance functionality and improve performance

--- page-about.php
+++ page-about.php
@@ -14,6 +14,38 @@ get_header()
         </div>
 
     </section>
+    <article class="text-slate-50 flex flex-col items-center sm:flex-row mt-20">
+        <div class="flex flex-col justify-center flex-shrink-0 items-center">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/profile.jpg" alt="Loc" class="mx-auto rounded-full flex-shrink-0 w-72 h-72 sm:w-80 sm:h-80 border-[5px] border-slate-50 object-cover mb-10" />
+            <ul class="flex gap-4 mb-4 text-slate-300 justify-center">
+                <?php
+                $social_media = [
+                    "facebook",
+                    "instagram",
+                    "twitter",
+                    "youtube",
+                    "linkedin",
+                ];
+                foreach ($social_media as $media) {
+                ?>
+                    <li>
+                        <a href="https://x.com/LaupWing1994" target="_blank">
+                            <?php
+                            get_template_part("templates/icons/$media");
+                            ?>
+                        </a>
+                    </li>
+                <?php
+                }
+                ?>
+            </ul>
+        </div>
+        <!-- show single blog post content -->
+        <div id="content" class="sm:ml-6">
+            <?php the_content(); ?>
+        </div>
+
+    </article>
 
 </div>
 <?php
