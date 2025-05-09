3d1d8df724b18fcd503551685409f52c588ff990
laupwing <laupwing@gmail.com>
2025-05-09 09:57:55 +0200

    Refactor sections to use template parts for improved maintainability in archive and front page

--- archive-product.php
+++ archive-product.php
@@ -4,15 +4,13 @@ get_header()
 <div class="custom-container flex items-start flex-col">
 
     <section class="flex flex-col py-8">
-        <div class="flex flex-col py-6">
-            <span class="text-slate-400 uppercase text-sm tracking-wider">Products</span>
-            <h3 class="transition-colors text-5xl pb-2 font-bold gradient-animation-slow bg-clip-text text-transparent">
-                Actually useful stuff
-            </h3>
-            <p class="max-w-4xl text-gray-700 md:text-lg 2xl:text-xl" data-fade="3">
-                Built from decades of experience in fitness, coding, and self-mastery — no fluff, just tools that work.
-            </p>
-        </div>
+        <?php
+        get_template_part('templates/sections/intro', null, [
+            'section_label' => 'Products',
+            'headline' => 'Actually useful stuff.',
+            'description' => 'Built from decades of experience in fitness, coding, and self-mastery — no fluff, just tools that work.'
+        ]);
+        ?>
         <ul class="grid mt-4 grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-6">
             <?php
             if (have_posts()) :
