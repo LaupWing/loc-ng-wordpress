be2ab45e620fe33dbd88e07c87f7e5875dff131c
laupwing <laupwing@gmail.com>
2025-05-09 08:40:59 +0200

    Update blog section header for clarity and engagement

--- page-blogs.php
+++ page-blogs.php
@@ -6,7 +6,7 @@ get_header()
         <div class="flex flex-col py-6">
             <span class="text-slate-400 uppercase text-sm tracking-wider">All Blogs</span>
             <h3 class="transition-colors text-5xl pb-2 font-bold gradient-animation-slow bg-clip-text text-transparent">
-                Actually useful stuff
+                Actionable. Real. Timeless.
             </h3>
             <p class="max-w-4xl text-gray-700 md:text-lg 2xl:text-xl" data-fade="3">
                 I share hard-earned insights on self-mastery, fitness, and spiritual growth — not theory, not trivia, just what actually works from real experience.
