84faca6be3e3b56045add5af705d1975c699d275
laupwing <laupwing@gmail.com>
2025-05-09 08:39:09 +0200

    Update about page content for clarity and engagement

--- page-about.php
+++ page-about.php
@@ -6,10 +6,10 @@ get_header()
         <div class="flex flex-col py-6">
             <span class="text-slate-400 uppercase text-sm tracking-wider">About</span>
             <h3 class="transition-colors text-5xl pb-2 font-bold gradient-animation-slow bg-clip-text text-transparent">
-                Actually useful stuff
+                Who is this guy?
             </h3>
             <p class="max-w-4xl text-gray-700 md:text-lg 2xl:text-xl" data-fade="3">
-                I share hard-earned insights on self-mastery, fitness, and spiritual growth — not theory, not trivia, just what actually works from real experience.
+                An gym bro turned software engineer, I’m on a mission to help you become the best version of yourself.
             </p>
         </div>
 
