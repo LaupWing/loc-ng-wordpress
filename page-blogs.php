0d630980d4da409fa72b33fa5e7c658efe5e9b27
laupwing <laupwing@gmail.com>
2025-05-09 09:59:22 +0200

    Refactor blog page to use template part for improved maintainability

--- page-blogs.php
+++ page-blogs.php
@@ -3,15 +3,13 @@ get_header()
 ?>
 <div class="custom-container flex items-start flex-col">
     <section class="flex flex-col py-8">
-        <div class="flex flex-col py-6">
-            <span class="text-slate-400 uppercase text-sm tracking-wider">All Blogs</span>
-            <h3 class="transition-colors text-5xl pb-2 font-bold gradient-animation-slow bg-clip-text text-transparent">
-                Actionable. Real. Timeless.
-            </h3>
-            <p class="max-w-4xl text-gray-700 md:text-lg 2xl:text-xl" data-fade="3">
-                I share hard-earned insights on self-mastery, fitness, and spiritual growth — not theory, not trivia, just what actually works from real experience.
-            </p>
-        </div>
+        <?php
+        get_template_part('templates/sections/intro', null, [
+            'section_label' => 'All Blogs',
+            'headline' => 'Actionable. Real. Timeless.',
+            'description' => 'I share hard-earned insights on self-mastery, fitness, and spiritual growth — not theory, not trivia, just what actually works from real experience.'
+        ]);
+        ?>
         <ul class="grid mt-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
             <?php
             $args = array(
