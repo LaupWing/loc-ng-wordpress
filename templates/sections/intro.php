227fdda06ac3a57ee6366454af3db9b31b7c3a76
laupwing <laupwing@gmail.com>
2025-05-11 17:44:33 +0200

    Enhance paragraph text color for dark mode support in intro section

--- templates/sections/intro.php
+++ templates/sections/intro.php
@@ -12,7 +12,7 @@ if ($fade_in) {
         <h3 class="transition-colors text-4xl md:text-5xl 2xl:text-6xl pb-2 font-bold gradient-animation-slow bg-clip-text text-transparent" data-fade="2">
             <?php echo esc_html($args['headline'] ?? 'Headline'); ?>
         </h3>
-        <p class="max-w-4xl text-gray-700 md:text-lg 2xl:text-xl" data-fade="3">
+        <p class="max-w-4xl text-gray-700 dark:text-gray-400 md:text-lg 2xl:text-xl" data-fade="3">
             <?php echo esc_html($args['description'] ?? 'Description'); ?>
         </p>
     </div>
@@ -27,7 +27,7 @@ if ($fade_in) {
         <h3 class="transition-colors text-4xl md:text-5xl 2xl:text-6xl pb-2 font-bold gradient-animation-slow bg-clip-text text-transparent">
             <?php echo esc_html($args['headline'] ?? 'Headline'); ?>
         </h3>
-        <p class="max-w-4xl text-gray-700 md:text-lg 2xl:text-xl">
+        <p class="max-w-4xl text-gray-700 dark:text-gray-400 md:text-lg 2xl:text-xl">
             <?php echo esc_html($args['description'] ?? 'Description'); ?>
         </p>
     </div>
