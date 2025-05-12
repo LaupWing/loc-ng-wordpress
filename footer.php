4473ab6951b2c3b4f87322c5e8466755b6cc1d12
laupwing <laupwing@gmail.com>
2025-05-12 10:06:37 +0200

    Add footer section with subscription form and introductory text

--- footer.php
+++ footer.php
@@ -1,3 +1,20 @@
+<section class="py-20 flex items-start flex-col">
+
+    <h1 class="font-bold text-black dark:text-white max-w-sm text-4xl md:text-5xl 2xl:text-6xl" data-fade="3">Do what works. Enjoy
+        <div class="inline-block text-light py-0.5">
+            <span class="transition-colors gradient-animation-slow bg-clip-text text-transparent">
+                Life.
+            </span>
+        </div>
+    </h1>
+    <p class="mt-4 max-w-4xl text-gray-700 dark:text-gray-400 md:mt-6 md:text-lg 2xl:text-xl" data-fade="4">Join 15,000+ becoming dangerous in body and mind.
+        Every week, I send raw, real insights on fitness, self-mastery, energy, and attraction — no bullsh*t, just what actually works.
+    </p>
+    <div data-fade="5" class="w-full mt-4">
+        <?php get_template_part('templates/sections/subscribe_form'); ?>
+    </div>
+</section>
+
 <footer class="mt-auto pt-20">
     <p class="text-slate-600 text-xl">© All Rights Reserved.</p>
 </footer>
