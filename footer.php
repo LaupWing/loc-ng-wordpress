19f1f1cdd5d125f4bdfb619d5b1616c38c0a881a
laupwing <laupwing@gmail.com>
2025-05-12 10:09:24 +0200

    Update footer section: Replace header with new text and adjust footer padding

--- footer.php
+++ footer.php
@@ -1,12 +1,8 @@
 <section class="py-20 flex items-start flex-col">
 
-    <h1 class="font-bold text-black dark:text-white max-w-sm text-4xl md:text-5xl 2xl:text-6xl" data-fade="3">Do what works. Enjoy
-        <div class="inline-block text-light py-0.5">
-            <span class="transition-colors gradient-animation-slow bg-clip-text text-transparent">
-                Life.
-            </span>
-        </div>
-    </h1>
+    <h3 class="transition-colors text-4xl md:text-5xl 2xl:text-6xl pb-2 font-bold gradient-animation-slow bg-clip-text text-transparent" data-fade="2">
+        Gain unique perspectives.
+    </h3>
     <p class="mt-4 max-w-4xl text-gray-700 dark:text-gray-400 md:mt-6 md:text-lg 2xl:text-xl" data-fade="4">Join 15,000+ becoming dangerous in body and mind.
         Every week, I send raw, real insights on fitness, self-mastery, energy, and attraction — no bullsh*t, just what actually works.
     </p>
@@ -15,7 +11,7 @@
     </div>
 </section>
 
-<footer class="mt-auto pt-20">
+<footer class="mt-auto pt-10">
     <p class="text-gray-300 dark:text-gray-700 text-xl">© All Rights Reserved.</p>
 </footer>
 </div>
