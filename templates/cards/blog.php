0be56b474f3afca85fda98b28275506073ac153a
laupwing <laupwing@gmail.com>
2025-05-15 09:16:44 +0200

    Refactor blog card template to enhance category display and structure
    - Wrapped category display in a div for better styling control.
    - Maintained existing logic for category background colors and names.
    - Improved readability by organizing the code structure.

--- templates/cards/blog.php
+++ templates/cards/blog.php
@@ -1,25 +1,28 @@
 <li class="border rounded-md flex flex-col overflow-hidden shadow border-gray-200 dark:border-gray-800">
     <a class="flex flex-col flex-1 gap-2 relative" href="<?php the_permalink(); ?>">
-        <!-- Get the categories -->
-        <?php
-        $categories = get_the_category();
-        $available_categories = [
-            "fitness" => "bg-emerald-600/60",
-            "coding" => "bg-blue-600/60",
-            "social-skills" => "bg-indigo-600/60",
-        ];
-        if ($categories) {
-            if (array_key_exists($categories[0]->slug, $available_categories)) {
-                echo '<span class="absolute top-4 left-4 ' . $available_categories[$categories[0]->slug] . ' text-white backdrop-blur-md px-2 py-1 rounded-md text-xs tracking-wider uppercase font-bold">' . $categories[0]->name . '</span>';
-            } else {
-                echo '<span class="absolute top-4 left-4  text-white px-2 py-1 rounded-md text-xs font-bold">' . $categories[0]->name . '</span>';
-            }
-        }
-        ?>
-        <?php if (has_post_thumbnail()) : ?>
-            <img class="aspect-video rounded-b-none object-cover" src="<?php the_post_thumbnail_url() ?>" alt="thumbnail photo">
+        <div class="relative dark:bg-gray-950 bg-gray-100">
 
-        <?php endif; ?>
+            <?php
+            $categories = get_the_category();
+            $available_categories = [
+                "fitness" => "bg-emerald-600/60",
+                "coding" => "bg-blue-600/60",
+                "social-skills" => "bg-indigo-600/60",
+            ];
+            if ($categories) {
+                if (array_key_exists($categories[0]->slug, $available_categories)) {
+                    echo '<span class="absolute top-4 left-4 ' . $available_categories[$categories[0]->slug] . ' text-white backdrop-blur-md px-2 py-1 rounded-md text-xs tracking-wider uppercase font-bold">' . $categories[0]->name . '</span>';
+                } else {
+                    echo '<span class="absolute top-4 left-4  text-white px-2 py-1 rounded-md text-xs font-bold">' . $categories[0]->name . '</span>';
+                }
+            }
+            ?>
+            <?php if (has_post_thumbnail()) : ?>
+                <img class="aspect-video rounded-b-none object-cover" src="<?php the_post_thumbnail_url() ?>" alt="thumbnail photo">
+
+            <?php endif; ?>
+        </div>
+        <!-- Get the categories -->
         <div class="flex flex-col flex-1 gap-2 p-4 pb-1">
             <h2 class="text-2xl text-gray-800 dark:text-white font-bold"><?php the_title(); ?></h2>
             <!-- get 100 charcaters of the contetn -->
