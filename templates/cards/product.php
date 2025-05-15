5b7ce9612452f1f31a219b55896e71e396ee8637
laupwing <laupwing@gmail.com>
2025-05-15 09:28:18 +0200

    Enhance product pricing display in product card template
    - Updated the pricing section to show sale prices with a strikethrough for original prices when applicable.
    - Added a conditional check for 'show_sale_price' to differentiate between regular and sale pricing.
    - Improved layout by wrapping pricing elements in a flex container for better alignment.

--- templates/cards/product.php
+++ templates/cards/product.php
@@ -9,13 +9,30 @@
             <img class="aspect-video rounded-lg object-cover" src="<?php the_post_thumbnail_url() ?>" alt="thumbnail photo">
 
         <?php endif; ?>
-        <?php if (get_field('is_free')) : ?>
-            <span class="absolute top-4 left-4 bg-emerald-600/60 text-white backdrop-blur-md px-2 py-1 rounded-md text-xs tracking-wider uppercase font-bold">Free</span>
-        <?php else : ?>
-            <span class="absolute top-4 left-4 bg-white/80 text-black backdrop-blur-md px-2 py-1 rounded-md tracking-wider uppercase font-bold">
-                $<?php echo get_field('price'); ?>
-            </span>
-        <?php endif; ?>
+        <div class="flex gap-2 items-center absolute top-4 left-4">
+            <?php if (get_field('is_free')) : ?>
+                <span class="bg-emerald-600/60 text-white backdrop-blur-md px-2 py-1 rounded-md text-xs tracking-wider uppercase font-bold">Free</span>
+            <?php else : ?>
+                <?php
+                if (get_field('show_sale_price')) {
+                ?>
+                    <span class="bg-white/60 line-through text-black backdrop-blur-md px-2 py-1 rounded-md tracking-wider uppercase font-bold">
+                        $<?php echo get_field('price'); ?>
+                    </span>
+                    <span class="text-green-700 bg-white/80 backdrop-blur-md px-2 py-1 rounded-md tracking-wider uppercase text-lg font-extrabold">
+                        $<?php echo get_field('sale_price'); ?>
+                    </span>
+                <?php
+                } else {
+                ?>
+                    <span class="bg-white/80 text-black backdrop-blur-md px-2 py-1 rounded-md tracking-wider uppercase font-bold">
+                        $<?php echo get_field('price'); ?>
+                    </span>
+                <?php
+                }
+                ?>
+            <?php endif; ?>
+        </div>
         <h2 class="text-2xl md:text-3xl 2xl:text-4xl text-gray-800 dark:text-gray-200 font-bold"><?php the_title(); ?></h2>
         <!-- get 100 charcaters of the contetn -->
         <p class="text-slate-400 text-lg md:text-xl 2xl:text-2xl">
